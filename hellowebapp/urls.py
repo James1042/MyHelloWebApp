18670312e2a98d71e82df1ff888b0583b18cc598
James <39966682+James1042@users.noreply.github.com>
2018-07-07 12:10:40 -0400

    HWA Code Ch10 - Installing Plugin -> Start filling templates

--- hellowebapp/urls.py
+++ hellowebapp/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.views.generic import TemplateView
 from collection import views
 
@@ -29,6 +29,7 @@ urlpatterns = [
     path('things/<slug>/', views.thing_detail,
 	 name='thing_detail'),
     path('things/<slug>/edit/', 
-	views.edit_thing, name='edit_thing'),    
+	views.edit_thing, name='edit_thing'),   
+    path('accounts/', include('registration.backends.simple.urls')), 
     path('admin/', admin.site.urls),
 ]

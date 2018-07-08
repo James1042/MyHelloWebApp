d328214f0120207d22e85b0e1416aa532c4c1b93
James <39966682+James1042@users.noreply.github.com>
2018-07-08 13:02:56 -0400

    HWA Code Ch10 - Setting up password reset

--- hellowebapp/urls.py
+++ hellowebapp/urls.py
@@ -17,6 +17,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
 from collection import views
+# import password stuff
+from django.contrib.auth.views import (
+    password_reset,
+    password_reset_done,
+    password_reset_confirm,
+    password_reset_complete,
+)
+
 
 urlpatterns = [
     path('', views.index, name='home'),
@@ -29,7 +37,24 @@ urlpatterns = [
     path('things/<slug>/', views.thing_detail,
 	 name='thing_detail'),
     path('things/<slug>/edit/', 
-	views.edit_thing, name='edit_thing'),   
+	views.edit_thing, name='edit_thing'),  
+    # the new password reset URLs
+    path('accounts/password/reset/', password_reset,
+       {'template_name': 'registration/password_reset_form.html'},
+       name='password_reset'),
+    path('accounts/password/reset/done',
+       password_reset_done,
+       {'template_name': 'registration/password_reset_done.html'},
+       name='password_reset_done'),
+    path('accounts/password/reset/<uidb64>/<token>/',
+       password_reset_confirm,
+       {'template_name': 'registration/password_reset_confirm.html'},
+       name='password_reset_confirm'),
+    path('accounts/password/done/',
+       password_reset_complete,
+       {'template_name': 'registration/password_reset_complete.html'},
+       name='password_reset_complete'), 
+    # end password reset URLs
     path('accounts/', include('registration.backends.simple.urls')), 
     path('admin/', admin.site.urls),
 ]
